eb3fd4e0569039db3418b9f107a1ac2143d8ec7f
GeertBoer <geert.boer555@gmail.com>
2016-12-15 22:30:02 +0100

    ENCODE WERKT! ...soort van... Het is een grote checkloze bende maar het idee is daar

--- StartPointForExercises/Assignments/Adidas/src/encode.c
+++ StartPointForExercises/Assignments/Adidas/src/encode.c
@@ -7,6 +7,99 @@
 #include "io.h"
 #include "parity.h"
 
+
+
+/*
+int check(uint8_t incomingByte)
+{
+	uint8_t MSN =>> incomingByte;
+	uint8_t LSB = (incomingByte =& 0x0F);
+
+}
+*/
+/*
+uint8_t check2(uint8_t incomingNibble)
+{
+	uint8_t incoming = incomingNibble;
+	uint8_t tmp = (incomingNibble << 4);
+	incoming |= tmp;
+
+	uint8_t mask = 0x07;
+	uint8_t amountOfSetBits = 0;
+	for (int i = 1; i <= 4; i++)
+	{
+		amountOfSetBits += (incoming & mask);
+		mask <<= 1;
+	}
+
+	return amountOfSetBits;
+}
+*/
+uint8_t checkParityBit(uint8_t byte, uint8_t parityBit)
+{
+	//0b00000110;
+	uint8_t offset = 0;
+	switch (parityBit)
+	{
+		case 0:
+			offset = 0;
+			break;
+		case 1:
+			offset = 2;
+			break;
+		case 2:
+			offset =1;
+			break;
+		default:
+			return -1;
+	}
+
+	uint8_t amount = 0;
+
+	uint8_t mask = 0x01;
+	mask <<= offset;
+	
+	for (int i = 1; i <= 3; i++)
+	{
+		printf("%d\n", mask);
+		if (mask & byte)
+		{
+			amount++;
+		}
+		mask <<= 1;
+	}
+
+	printf("Amount: %d\n", amount);
+	return (amount & 0x01);
+}
+
+uint8_t checkNibble(uint8_t byte, uint8_t *parityArray)
+{
+	uint8_t byteToSend = byte;
+	byteToSend |= (byte << 4);
+
+	for (int i = 0; i < 3; i++)
+	{
+		parityArray[i] = checkParityBit(byteToSend, i);
+	}
+
+	return 0;
+}
+
+uint8_t checkByte(uint8_t byte, uint8_t *lsbParity, uint8_t *msbParity)
+{
+	uint8_t LSB = byte & 0x0F;
+	uint8_t MSB = (byte & 0xF0) >> 4;
+
+	checkNibble(LSB, lsbParity);
+	checkNibble(MSB, msbParity);
+
+	return 0;
+}
+
+//size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
+
+
 extern int 
 encode(int argc, char * argv[])
 {
@@ -37,35 +130,23 @@ encode(int argc, char * argv[])
     printf("%d\n", byteArray[0]);
     
     return (amountOfReadItems);
-    */
+	*/
+	uint8_t lsbParity[3];
+	uint8_t msbParity[3];
+
+	uint8_t byte = 0b01101110;
+	uint8_t check = checkByte(byte, lsbParity, msbParity);
+
+	for (int i = 0; i <= 2; i++)
+	{
+		printf("LSB: p %d %d\n", i, lsbParity[i]);
+	}
+	for (int i = 0; i <= 2; i++)
+	{
+		printf("MSB: p %d %d\n", i, msbParity[i]);
+	}
+
+	printf("result: %d\n", check);
 
     return (0);
 }
-
-/*
-int check(uint8_t incomingByte)
-{
-	uint8_t MSN =>> incomingByte;
-	uint8_t LSB = (incomingByte =& 0x0F);
-
-}
-*/
-
-uint8_t check2(uint8_t incomingNibble)
-{
-	uint8_t incoming = incomingNibble;
-	uint8_t tmp = (incomingNibble << 4);
-	incoming |= tmp;
-
-	uint8_t mask = 0x07;
-	uint8_t amountOfSetBits = 0;
-	for (int i = 1; i <= 4; i++)
-	{
-		amountOfSetBits += (incoming & mask);
-		mask <<= 1;
-	}
-
-	return amountOfSetBits;
-}
-
-//size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
